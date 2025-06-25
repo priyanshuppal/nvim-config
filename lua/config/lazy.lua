fa2ae1d79d429d8a5cb68e3a5eedd701a8282222
Pri Uppal <Pri.Uppal@admin.abs.gov.au>
2025-06-25 16:29:03 +0930

    removed notifs and added custom keys to blink

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,9 +27,17 @@ require("lazy").setup({
 		-- import your plugins
 		{ import = "plugins" },
 	},
+    checker = {
+        notify = false,
+        enabled = true,
+        frequency = 3600,
+    },
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+    change_detection = {
+        enabled = true,
+        notify = false,
+    },
 })
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -8,7 +8,26 @@ return {
     ---@type blink.cmp.Config
     opts = {
         keymap = {
-            preset = "super-tab",
+            preset = "none",
+            ["<Up>"] = { "select_prev", "fallback" },
+            ["<Down>"] = { "select_next", "fallback" },
+            ["<CR>"] = { "accept", "fallback" }, -- show with a list of providers
+            ["<C-space>"] = {
+                function(cmp)
+                    cmp.show()
+                end,
+            },
+
+            -- control whether the next command will be run when using a function
+            ["<C-n>"] = {
+                function(cmp)
+                    if some_condition then
+                        return
+                    end -- runs the next command
+                    return true -- doesn't run the next command
+                end,
+                "select_next",
+            },
         },
         appearance = {
             nerd_font_variant = "mono",
