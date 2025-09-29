b804154568b17707c50c06fc2b4c51dd67a60d64
Pri Uppal <Pri.Uppal@admin.abs.gov.au>
2025-09-29 15:58:21 +0930

    better

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,50 +1,52 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-                close_if_last_window = false,
-				filtered_items = {
-					hide_dotfiles = false,
-				},
-				follow_current_file = {
-					enabled = true, -- This will find and focus the file in the active buffer every time
-					--               -- the current file is changed while the tree is open.
-					leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-				},
-			},
-			buffers = {
-				follow_current_file = {
-					enabled = true, -- This will find and focus the file in the active buffer every time
-					--              -- the current file is changed while the tree is open.
-					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-				},
-			},
-			window = {
-				position = "right",
-			},
-			setup_called = false,
-            event_handlers = {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        close_if_last_window = false,
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+        follow_current_file = {
+          enabled = true,         -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true,          -- This will find and focus the file in the active buffer every time
+          --              -- the current file is changed while the tree is open.
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      },
+      window = {
+        position = "right",
+        mappings = {
+          [";"] = "open",
+        },
+      },
+      setup_called = false,
+      event_handlers = {
 
-          {
-            event = "file_open_requested",
-            handler = function()
-              -- auto close
-              -- vim.cmd("Neotree close")
-              -- OR
-              require("neo-tree.command").execute({ action = "close" })
-            end
-          },
-
-        }
-		})
-		vim.keymap.set("n", "<leader>ee", ":Neotree toggle filesystem right<CR>", { desc = "Explorer" })
-		vim.keymap.set("n", "<leader>ef", ":Neotree toggle reveal right<CR>", { desc = "Reveal with current file" })
-	end,
+        {
+          event = "file_open_requested",
+          handler = function()
+            -- auto close
+            -- vim.cmd("Neotree close")
+            -- OR
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+    })
+    vim.keymap.set("n", "<leader>ee", ":Neotree toggle filesystem right<CR>", { desc = "Explorer" })
+    vim.keymap.set("n", "<leader>ef", ":Neotree toggle reveal right<CR>", { desc = "Reveal with current file" })
+  end,
 }
